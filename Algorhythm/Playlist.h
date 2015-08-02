fbc9adc7668ef68805e1b4dbe4e83080d1715897
Josh <Joshkmotley@gmail.com>
2015-08-02 17:23:29 -0400

    create playlist model to use music library model info in playlist instance

---
+++ Algorhythm/Playlist.h
@@ -0,0 +1,22 @@
+//
+//  Playlist.h
+//  Algorhythm
+//
+//  Created by Joshua Motley on 8/1/15.
+//  Copyright (c) 2015 TeamTreeHouse. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface Playlist : NSObject
+
+@property (strong, nonatomic) NSString *playlistTitle;
+@property (strong, nonatomic) NSString *playlistDescription;
+@property (strong, nonatomic) UIImage *playlistIcon;
+@property (strong, nonatomic) UIImage *playlistLargeIcon;
+@property (strong, nonatomic) NSArray *playlistArtists;
+@property (strong, nonatomic) UIColor *playlistBackgroundColor;
+
+
+@end
---
+++ Algorhythm/Playlist.m
@@ -0,0 +1,13 @@
+//
+//  Playlist.m
+//  Algorhythm
+//
+//  Created by Joshua Motley on 8/1/15.
+//  Copyright (c) 2015 TeamTreeHouse. All rights reserved.
+//
+
+#import "Playlist.h"
+
+@implementation Playlist
+
+@end

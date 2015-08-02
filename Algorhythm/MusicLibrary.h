dd89094753ec0a705fa093e47d6fcbcc1f2c44ce
Josh <Joshkmotley@gmail.com>
2015-08-01 22:52:53 -0400

    create data model with an array of dictionaries(playlists)

---
+++ Algorhythm/MusicLibrary.h
@@ -0,0 +1,15 @@
+//
+//  MusicLibrary.h
+//  Algorhythm
+//
+//  Created by Joshua Motley on 8/1/15.
+//  Copyright (c) 2015 TeamTreeHouse. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MusicLibrary : NSObject
+
+@property (strong, nonatomic) NSArray *library;
+
+@end
